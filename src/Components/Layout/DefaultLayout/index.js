0e41320bc67120a2dbb7a0526388ad78a2ad3a9b
tuna <tuanzkt2711@gmail.com>
2025-11-19 23:31:33 +0700

    base project

---
+++ src/Components/Layout/DefaultLayout/index.js
@@ -0,0 +1,19 @@
+import Header from '~/Components/Layout/Component/Header';
+import SideBar from './SideBar';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <SideBar />
+                <div className={cx('content')}>{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
